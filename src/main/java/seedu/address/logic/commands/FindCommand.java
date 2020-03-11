922920164b48c4c45e39edf7708333137d2e0026
Hanming Zhu <hanming@huawen.com>
2020-03-12 04:32:48 +0800

    style: fix command code to fit checkstyle requirements

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import seedu.address.commons.core.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 
@@ -30,7 +29,7 @@ public class FindCommand extends Command {
         requireNonNull(model);
         //model.updateFilteredPersonList(predicate);
         //return new CommandResult(
-         //       String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+        //       String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
         return null;
     }
 
