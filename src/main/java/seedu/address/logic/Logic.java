cf645d00d39badf6c893d9d118e59750e221d540
Hanming Zhu <hanming@huawen.com>
2020-03-12 04:32:55 +0800

    style: fix logic code to fit checkstyle requirements

--- src/main/java/seedu/address/logic/Logic.java
+++ src/main/java/seedu/address/logic/Logic.java
@@ -17,10 +17,11 @@ import seedu.address.model.entity.CalendarItem;
 public interface Logic {
     /**
      * Executes the command and returns the result.
+     *
      * @param commandText The command as entered by the user.
      * @return the result of the command execution.
      * @throws CommandException If an error occurs during command execution.
-     * @throws ParseException If an error occurs during parsing.
+     * @throws ParseException   If an error occurs during parsing.
      */
     CommandResult execute(String commandText) throws CommandException, ParseException;
 
@@ -31,7 +32,9 @@ public interface Logic {
      */
     ReadOnlyCalendar getCalendar();
 
-    /** Returns an unmodifiable view of the filtered list of persons */
+    /**
+     * Returns an unmodifiable view of the filtered list of persons
+     */
     ObservableList<CalendarItem> getFilteredCalendarItemList();
 
     /**
