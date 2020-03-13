62a207724387f2a4f3c507096a5b1d3331c052c6
seanlowcy77 <seanlowcy77@Seans-MacBook-Pro-4.local>
2020-03-14 04:11:44 +0800

    Change variables to accomodate Calendar Items

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -4,7 +4,7 @@ import static java.util.Objects.requireNonNull;
 
 import seedu.address.model.Model;
 
-//import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_CALENDAR_ITEMS;
 
 /**
  * Lists all persons in the address book to the user.
@@ -13,12 +13,12 @@ public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String MESSAGE_SUCCESS = "Listed all Module Events";
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        //model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        model.updateFilteredCalendarItemList(PREDICATE_SHOW_ALL_CALENDAR_ITEMS);
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
