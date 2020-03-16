0579e75b8242be7883565684c94c91038d311853
Hanming Zhu <hanming@huawen.com>
2020-03-16 23:56:03 +0800

    feat: add displayable
    Introduced the concept of Displayable for the Events and Modules to implement.

---
+++ src/main/java/seedu/address/model/Displayable.java
@@ -0,0 +1,7 @@
+package seedu.address.model;
+
+/**
+ * An interface for classes that can be listed in Modulo.
+ */
+public interface Displayable {
+}
---
+++ src/main/java/seedu/address/model/DisplayableType.java
@@ -0,0 +1,9 @@
+package seedu.address.model;
+
+/**
+ * Type of {@code Displayable}s in the app.
+ */
+public enum DisplayableType {
+    MODULE,
+    EVENT
+}
