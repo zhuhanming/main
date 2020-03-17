5f074b59d24e23a2688ab792035cefb7df1525b5
Hanming Zhu <hanming@huawen.com>
2020-03-17 14:03:10 +0800

    feat: add a collector to collection util
    This collector extracts a single element from the given stream.

--- src/main/java/seedu/address/commons/util/CollectionUtil.java
+++ src/main/java/seedu/address/commons/util/CollectionUtil.java
@@ -5,14 +5,22 @@ import static java.util.Objects.requireNonNull;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Objects;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import seedu.address.model.event.Event;
+import seedu.address.model.event.exceptions.DuplicateEventException;
+import seedu.address.model.module.exceptions.DuplicateModuleException;
 
 /**
  * Utility methods related to Collections
  */
 public class CollectionUtil {
 
-    /** @see #requireAllNonNull(Collection) */
+    /**
+     * @see #requireAllNonNull(Collection)
+     */
     public static void requireAllNonNull(Object... items) {
         requireNonNull(items);
         Stream.of(items).forEach(Objects::requireNonNull);
@@ -31,5 +39,21 @@ public class CollectionUtil {
      */
     public static boolean isAnyNonNull(Object... items) {
         return items != null && Arrays.stream(items).anyMatch(Objects::nonNull);
+    }
+
+    /**
+     * Collects the stream into a single element.
+     */
+    public static <T> Collector<T, ?, T> toSingleton() {
+        return Collectors.collectingAndThen(Collectors.toList(), list -> {
+            if (list.size() != 1) {
+                if (list.get(0) instanceof Module) {
+                    throw new DuplicateModuleException();
+                } else if (list.get(0) instanceof Event) {
+                    throw new DuplicateEventException();
+                }
+            }
+            return list.get(0);
+        });
     }
 }
