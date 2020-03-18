81f6229d863f4be8dc09dc13619b6d3682eeb0a4
seanlowcy77 <seanlowcy77@Seans-MacBook-Pro-4.local>
2020-03-18 23:26:22 +0800

    Change ListCommand from event to general usage

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,23 +1,30 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_EVENTS;
 
+import seedu.address.model.DisplayableType;
 import seedu.address.model.Model;
 
 /**
  * Lists all persons in the address book to the user.
  */
 public class ListCommand extends Command {
+    private DisplayableType displayableType;
 
     public static final String COMMAND_WORD = "list";
 
     public static final String MESSAGE_SUCCESS = "Listed all Module Events";
 
+    public ListCommand(DisplayableType displayableType) {
+        this.displayableType = displayableType;
+    }
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredEventList(PREDICATE_SHOW_ALL_EVENTS);
+
+        System.out.println(displayableType.toString());
+        model.setFilteredFocusedList(displayableType);
+        //model.updateFilteredEventList(PREDICATE_SHOW_ALL_EVENTS);
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
