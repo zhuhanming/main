0d3be8abf33a9d060c6b43f2935066bd39f4aee3
seanlowcy77 <seanlowcy77@Seans-MacBook-Pro-4.local>
2020-03-21 13:23:46 +0800

    Change ListCommand to show event and module list

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -9,22 +9,37 @@ import seedu.address.model.Model;
  * Lists all persons in the address book to the user.
  */
 public class ListCommand extends Command {
-    private DisplayableType displayableType;
 
     public static final String COMMAND_WORD = "list";
-
-    public static final String MESSAGE_SUCCESS = "Listed all Module Events";
+    public static final String MODULE_MESSAGE_SUCCESS = "Listed all Modules";
+    public static final String EVENT_MESSAGE_SUCCESS = "Listed all Module Events";
+    public static final String MESSAGE_SUCCESS = "Lists";
+    private DisplayableType displayableType;
 
     public ListCommand(DisplayableType displayableType) {
         this.displayableType = displayableType;
     }
+
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-
-        System.out.println(displayableType.toString());
         model.setFilteredFocusedList(displayableType);
-        //model.updateFilteredEventList(PREDICATE_SHOW_ALL_EVENTS);
-        return new CommandResult(MESSAGE_SUCCESS);
+        CommandResult commandResult = returnCommResultBasedOnType(displayableType);
+        return commandResult;
+    }
+
+    /**
+     * Returns a CommandResult based on the displayable type.
+     * @param displayableType the displayable type of the object.
+     * @return a CommandResult based on the displayable type.
+     */
+    private CommandResult returnCommResultBasedOnType(DisplayableType displayableType) {
+        if (displayableType == DisplayableType.EVENT) {
+            return new CommandResult(EVENT_MESSAGE_SUCCESS, false, false, true, false);
+        } else if (displayableType == DisplayableType.MODULE) {
+            return new CommandResult(MODULE_MESSAGE_SUCCESS, false, false, false, true);
+        } else {
+            return new CommandResult(MESSAGE_SUCCESS);
+        }
     }
 }
