b2b363a7c40511eb8d49a0058c413fd030d8f375
seanlowcy77 <seanlowcy77@Seans-MacBook-Pro-4.local>
2020-03-26 11:39:09 +0800

    Move file to predicate directory

---
+++ src/main/java/seedu/address/model/predicate/NameContainsKeywordsPredicate.java
@@ -0,0 +1,34 @@
+package seedu.address.model.predicate;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.model.Displayable;
+
+/**
+ * Tests that a {@code Person}'s {@code CalendarItemName} matches any of the keywords given.
+ */
+public class NameContainsKeywordsPredicate implements Predicate<Displayable> {
+    private final List<String> keywords;
+
+    public NameContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Displayable displayable) {
+        for (String keyword : keywords) {
+            if (!displayable.findCommandString().trim().toLowerCase().contains(keyword)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof NameContainsKeywordsPredicate // instanceof handles nulls
+                && keywords.equals(((NameContainsKeywordsPredicate) other).keywords)); // state check
+    }
+}
