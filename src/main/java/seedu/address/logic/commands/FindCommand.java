7e66a263221dfc406171c69fed52779ea6169a24
seanlowcy77 <seanlowcy77@r-4-98-25-172.comp.nus.edu.sg>
2020-03-26 09:48:23 +0800

    Change execute function

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -12,7 +12,7 @@ import seedu.address.model.person.NameContainsKeywordsPredicate;
 public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
-
+    public static final String MESSAGE_SUCCESS = "Found";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
@@ -27,10 +27,9 @@ public class FindCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        //model.updateFilteredPersonList(predicate);
-        //return new CommandResult(
-        //       String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
-        return null;
+        model.updateFilteredDisplayableList(predicate);
+        return new CommandResult(
+               String.format(MESSAGE_SUCCESS, model.getFilteredFocusedList().size()));
     }
 
     @Override
