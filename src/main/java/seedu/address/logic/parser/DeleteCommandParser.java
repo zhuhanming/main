635eb6c2ed8b8d839c82749f9c056fe59d3d6a36
seanlowcy77 <seanlowcy77@Seans-MacBook-Pro-4.local>
2020-03-30 00:46:59 +0800

    Add parsing for delete command input

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -4,7 +4,11 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.predicate.NameContainsKeywordsPredicate;
+
+import java.util.Arrays;
 
 /**
  * Parses input arguments and creates a new DeleteCommand object
@@ -14,17 +18,24 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the DeleteCommand
      * and returns a DeleteCommand object for execution.
-     *
-     * @throws ParseException if the user input does not conform the expected format
      */
-    public DeleteCommand parse(String args) throws ParseException {
+    public DeleteCommand parse(String args) throws ParseException{
+        if (args.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+        }
         try {
             Index index = ParserUtil.parseIndex(args);
             return new DeleteCommand(index);
         } catch (ParseException pe) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
+            String trimmedArgs = args.toLowerCase().trim();
+            if (trimmedArgs.equals("all")) {
+                return new DeleteCommand(new NameContainsKeywordsPredicate(Arrays.asList("")));
+            }
+            String[] nameKeywords = {trimmedArgs};
+            return new DeleteCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
         }
+
     }
 
 }
