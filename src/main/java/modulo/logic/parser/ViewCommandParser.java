5fa43bda02294864f07f9a5eac50b7a52ee4fb50
Hanming Zhu <hanming@huawen.com>
2020-04-01 20:52:12 +0800

    docs+refactor(ViewCommandParser): do javadocs and touch up

--- src/main/java/modulo/logic/parser/ViewCommandParser.java
+++ src/main/java/modulo/logic/parser/ViewCommandParser.java
@@ -2,8 +2,6 @@ package modulo.logic.parser;
 
 import static modulo.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import java.util.stream.Stream;
-
 import modulo.commons.core.index.Index;
 import modulo.logic.commands.ViewCommand;
 import modulo.logic.parser.exceptions.ParseException;
@@ -14,9 +12,11 @@ import modulo.logic.parser.exceptions.ParseException;
  */
 public class ViewCommandParser implements Parser<ViewCommand> {
     /**
-     * Parses the given {@code String} of arguments in the context of the AddCommand and returns an AddCommand object
+     * Parses the given {@code String} of arguments in the context of the ViewCommand and returns a ViewCommand object
      * for execution.
      *
+     * @param args Arguments passed in by the user.
+     * @return {@code ViewCommand} to execute.
      * @throws ParseException if the user input does not conform the expected format
      */
     public ViewCommand parse(String args) throws ParseException {
@@ -27,14 +27,6 @@ public class ViewCommandParser implements Parser<ViewCommand> {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewCommand.MESSAGE_USAGE), pe);
         }
         return new ViewCommand(index);
-    }
-
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given {@code
-     * ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 }
 
