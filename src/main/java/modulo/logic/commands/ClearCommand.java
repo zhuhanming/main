8b2ac4ed53fc835139d30780ce9d755176145e86
Hanming Zhu <hanming@huawen.com>
2020-04-01 20:46:36 +0800

    docs+refactor(ClearCommand): do javadocs and touch up

--- src/main/java/modulo/logic/commands/ClearCommand.java
+++ src/main/java/modulo/logic/commands/ClearCommand.java
@@ -1,22 +1,22 @@
 package modulo.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static modulo.commons.core.Messages.MESSAGE_MODULO_CLEARED;
 
 import modulo.model.Model;
 
 /**
- * Clears the address book.
+ * Clears Modulo.
  */
 public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
-    public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         //model.setAddressBook(new AddressBook());
-        return new CommandResult(MESSAGE_SUCCESS);
+        return new CommandResult(MESSAGE_MODULO_CLEARED);
     }
 }
