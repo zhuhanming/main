c47539318a0579929ae93f075721b6a1ff3bab56
Hanming Zhu <hanming@huawen.com>
2020-04-01 20:49:46 +0800

    docs+refactor(AddDeadlineCommandParser): do javadocs and touch up

--- src/main/java/modulo/logic/parser/AddDeadlineCommandParser.java
+++ src/main/java/modulo/logic/parser/AddDeadlineCommandParser.java
@@ -6,6 +6,7 @@ import static modulo.logic.parser.CliSyntax.PREFIX_MODULE;
 import static modulo.logic.parser.CliSyntax.PREFIX_NAME;
 import static modulo.logic.parser.CliSyntax.PREFIX_REPEAT;
 
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 import modulo.logic.commands.AddDeadlineCommand;
@@ -26,40 +27,46 @@ import modulo.model.module.PartialModule;
 public class AddDeadlineCommandParser implements Parser<AddDeadlineCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the AddCommand and returns an AddCommand object
-     * for execution.
+     * Parses the given {@code String} of arguments in the context of the AddDeadlineCommand and returns an
+     * AddDeadlineCommand object for execution.
      *
-     * @throws ParseException if the user input does not conform the expected format
+     * @param args Arguments passed in by the user.
+     * @return {@code AddDeadlineCommand} to execute.
+     * @throws ParseException if the user input does not conform the expected format.
      */
     public AddDeadlineCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_MODULE, PREFIX_EVENT, PREFIX_NAME, PREFIX_REPEAT);
 
-        if (!arePrefixesPresent(argMultimap, PREFIX_NAME)
-                || !argMultimap.getPreamble().isEmpty()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddDeadlineCommand.MESSAGE_USAGE));
+        Supplier<ParseException> parseExceptionSupplier = () -> new ParseException(String.format(
+                MESSAGE_INVALID_COMMAND_FORMAT, AddDeadlineCommand.MESSAGE_USAGE));
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME) || !argMultimap.getPreamble().isEmpty()) {
+            parseExceptionSupplier.get();
         }
 
-        boolean isRepeated = false;
-        if (argMultimap.getValue(PREFIX_REPEAT).isPresent()) {
-            isRepeated = ParserUtil.parseRepeat(argMultimap.getValue(PREFIX_REPEAT).get());
-        }
-        Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
+        boolean isRepeated = ParserUtil.parseRepeat(argMultimap.getValue(PREFIX_REPEAT).orElse("NO"));
+
+        Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).orElseThrow(parseExceptionSupplier));
 
         ModuleCode moduleCode = null;
         if (argMultimap.getValue(PREFIX_MODULE).isPresent()) {
             moduleCode = ParserUtil.parseModuleCode(argMultimap.getValue(PREFIX_MODULE).get());
         }
+
         Name eventName = null;
-        if (argMultimap.getValue(PREFIX_MODULE).isPresent()) {
+        if (argMultimap.getValue(PREFIX_EVENT).isPresent()) {
             eventName = ParserUtil.parseName(argMultimap.getValue(PREFIX_EVENT).get());
         }
 
         if (moduleCode == null && eventName == null) {
             return new AddDeadlineCommand(name, isRepeated);
+        } else if (moduleCode == null || eventName == null) {
+            parseExceptionSupplier.get();
         }
 
-        EventType eventType = ParserUtil.parseEventType(name.toString());
+        assert eventName != null;
+        EventType eventType = ParserUtil.parseEventType(eventName.toString());
 
         Module module = new PartialModule(moduleCode);
         Event event = new PartialEvent(eventName, eventType, module, new Location("Arbitrary Location"));
@@ -70,6 +77,10 @@ public class AddDeadlineCommandParser implements Parser<AddDeadlineCommand> {
     /**
      * Returns true if none of the prefixes contains empty {@code Optional} values in the given {@code
      * ArgumentMultimap}.
+     *
+     * @param argumentMultimap Multimap containing the arguments.
+     * @param prefixes         Prefixes to check.
+     * @return Boolean denoting whether the prefixes are present.
      */
     private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
         return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
