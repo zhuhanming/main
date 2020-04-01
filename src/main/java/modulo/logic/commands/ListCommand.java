b4712490ad23705089d7d8e65efaf85c88ab1481
Hanming Zhu <hanming@huawen.com>
2020-04-01 20:48:13 +0800

    docs+refactor(ListCommand): do javadocs and touch up

--- src/main/java/modulo/logic/commands/ListCommand.java
+++ src/main/java/modulo/logic/commands/ListCommand.java
@@ -1,23 +1,40 @@
 package modulo.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static modulo.commons.core.Messages.MESSAGE_SHOWING_ALL_EVENTS;
+import static modulo.commons.core.Messages.MESSAGE_SHOWING_ALL_MODULES;
+import static modulo.commons.core.Messages.MESSAGE_SHOWING_EVENTS;
+import static modulo.commons.core.Messages.MESSAGE_SHOWING_MODULES;
 
 import modulo.model.DisplayableType;
 import modulo.model.Model;
 
 /**
- * Lists all persons in the address book to the user.
+ * Displays the specified list to the user.
  */
 public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
-    public static final String MODULE_MESSAGE_SUCCESS = "Showing Modules!";
-    public static final String EVENT_MESSAGE_SUCCESS = "Showing Events!";
-    public static final String MODULE_SHOW_ALL_MESSAGE_SUCCESS = "Showing All Modules!";
-    public static final String EVENT_SHOW_ALL_MESSAGE_SUCCESS = "Showing All Events!";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists out the specified items.\n"
+            + "Parameters: "
+            + "[all] "
+            + "event/module "
+            + "\n"
+            + "Example: " + COMMAND_WORD + " all event";
+
     private DisplayableType displayableType;
+    /**
+     * Whether to show all elements of specified type.
+     */
     private boolean toShowAll;
 
+    /**
+     * Creates a {@code ListCommand} that lists out the items of the given displayable type.
+     *
+     * @param displayableType Type of item to display.
+     * @param toShowAll       Whether to show all of that type.
+     */
     public ListCommand(DisplayableType displayableType, boolean toShowAll) {
         this.displayableType = displayableType;
         this.toShowAll = toShowAll;
@@ -28,12 +45,12 @@ public class ListCommand extends Command {
         requireNonNull(model);
         if (toShowAll) {
             model.setFilteredFocusedListShowAll(displayableType);
-            return new CommandResult(displayableType == DisplayableType.MODULE ? MODULE_SHOW_ALL_MESSAGE_SUCCESS
-                    : EVENT_SHOW_ALL_MESSAGE_SUCCESS, false, false, true, false, null);
+            return new CommandResult(displayableType == DisplayableType.MODULE ? MESSAGE_SHOWING_ALL_MODULES
+                    : MESSAGE_SHOWING_ALL_EVENTS, false, false, true, false, null);
         } else {
             model.setFilteredFocusedList(displayableType);
-            return new CommandResult(displayableType == DisplayableType.MODULE ? MODULE_MESSAGE_SUCCESS
-                    : EVENT_MESSAGE_SUCCESS, false, false, true, false, null);
+            return new CommandResult(displayableType == DisplayableType.MODULE ? MESSAGE_SHOWING_MODULES
+                    : MESSAGE_SHOWING_EVENTS, false, false, true, false, null);
         }
 
     }
