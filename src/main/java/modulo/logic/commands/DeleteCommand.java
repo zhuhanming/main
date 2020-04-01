a27a4f8161a45d1bd336ce0df6edad33c684fbe1
seanlowcy77 <seanlowcy77@Seans-MacBook-Pro-4.local>
2020-04-01 19:45:00 +0800

    feature: Add functionality to delete deadlines

--- src/main/java/modulo/logic/commands/DeleteCommand.java
+++ src/main/java/modulo/logic/commands/DeleteCommand.java
@@ -9,6 +9,7 @@ import modulo.logic.commands.exceptions.CommandException;
 import modulo.model.Displayable;
 import modulo.model.DisplayableType;
 import modulo.model.Model;
+import modulo.model.deadline.Deadline;
 import modulo.model.event.Event;
 import modulo.model.module.Module;
 import modulo.model.predicate.NameContainsKeywordsPredicate;
@@ -20,7 +21,6 @@ public class DeleteCommand extends Command {
 
     public static final String COMMAND_WORD = "delete";
     private static final String predicateStringForDeleteAll = "\"\"";
-
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Deletes the module or event identified by the index number used in the displayed list.\n"
             + "Parameters: INDEX (must be a positive integer)\n"
@@ -29,15 +29,18 @@ public class DeleteCommand extends Command {
 
     private final Index targetIndex;
     private final NameContainsKeywordsPredicate predicate;
+    private final boolean isDeadline;
 
-    public DeleteCommand(Index targetIndex) {
+    public DeleteCommand(Index targetIndex, boolean isDeadline) {
         this.targetIndex = targetIndex;
         this.predicate = null;
+        this.isDeadline = isDeadline;
     }
 
     public DeleteCommand(NameContainsKeywordsPredicate predicate) {
         this.targetIndex = null;
         this.predicate = predicate;
+        this.isDeadline = false;
     }
 
     @Override
@@ -45,25 +48,48 @@ public class DeleteCommand extends Command {
         requireNonNull(model);
         DisplayableType displayableType = model.getCurrentDisplayableType();
         if (targetIndex != null) {
-            ObservableList<? extends Displayable> lastShownList = model.getFilteredFocusedList();
-            if (targetIndex.getZeroBased() >= lastShownList.size()) {
-                throw new CommandException(Messages.MESSAGE_INVALID_DELETE_INDEX);
-            }
+            if (!isDeadline) {
+                ObservableList<? extends Displayable> lastShownList = model.getFilteredFocusedList();
+                if (targetIndex.getZeroBased() >= lastShownList.size()) {
+                    throw new CommandException(Messages.MESSAGE_INVALID_DELETE_INDEX);
+                }
 
-            Displayable itemToDelete = lastShownList.get(targetIndex.getZeroBased());
-            unsetFocusDisplayableIfEqual(model, itemToDelete);
+                Displayable itemToDelete = lastShownList.get(targetIndex.getZeroBased());
+                unsetFocusDisplayableIfEqual(model, itemToDelete);
 
-            if (displayableType == DisplayableType.EVENT) {
-                Event event = (Event) itemToDelete;
-                model.deleteEvent(event);
-                return new CommandResult(String.format(Messages.MESSAGE_SINGLE_EVENT_DELETE_SUCCESS, itemToDelete),
-                        false, false, true, true, null);
-            } else if (displayableType == DisplayableType.MODULE) {
-                Module module = (Module) itemToDelete;
-                deleteEventsOfModule(model, module);
-                model.deleteModule((Module) itemToDelete);
-                return new CommandResult(String.format(Messages.MESSAGE_SINGLE_MODULE_DELETE_SUCCESS, itemToDelete),
-                        false, false, true, true, null);
+                if (displayableType == DisplayableType.EVENT) {
+                    Event event = (Event) itemToDelete;
+                    model.deleteEvent(event);
+                    return new CommandResult(String.format(Messages.MESSAGE_SINGLE_EVENT_DELETE_SUCCESS, itemToDelete),
+                            false, false, true, true, null);
+                } else if (displayableType == DisplayableType.MODULE) {
+                    Module module = (Module) itemToDelete;
+                    deleteEventsOfModule(model, module);
+                    model.deleteModule((Module) itemToDelete);
+                    return new CommandResult(String.format(Messages.MESSAGE_SINGLE_MODULE_DELETE_SUCCESS, itemToDelete),
+                            false, false, true, true, null);
+                }
+            } else {
+                try {
+                    Event displayedEvent = (Event) model.getFocusedDisplayable();
+                    int index = targetIndex.getZeroBased();
+                    Deadline itemToDelete = displayedEvent.getDeadlines().get(index);
+                    displayedEvent.removeDeadline(itemToDelete);
+                    return (itemToDelete.isCompleted()) ? new CommandResult(String.format(
+                            Messages.MESSAGE_COMPLETED_DEADLINE_DELETE_SUCCESS, itemToDelete),
+                            false, false, true, true, null)
+                            : new CommandResult(String.format(
+                                    Messages.MESSAGE_INCOMPLETE_DEADLINE_DELETE_SUCCESS, itemToDelete),
+                                    false, false, true, true, null);
+
+
+                } catch (ClassCastException e) {
+                    return new CommandResult(Messages.MESSAGE_EVENT_NOT_SELECTED);
+                } catch (NullPointerException e) {
+                    return new CommandResult(Messages.MESSAGE_EVENT_NOT_SELECTED);
+                } catch (IndexOutOfBoundsException e) {
+                    return new CommandResult(Messages.MESSAGE_INVALID_DELETE_INDEX);
+                }
             }
         } else {
             Object[] list = model.getFilteredDisplayableList(predicate);
