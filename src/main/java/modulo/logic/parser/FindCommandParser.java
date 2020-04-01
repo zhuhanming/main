087ac396d5d0c9c31bebd33bf128f4b78a0b5301
Hanming Zhu <hanming@huawen.com>
2020-04-01 20:51:04 +0800

    docs+refactor(FindCommandParser): do javadocs and touch up

--- src/main/java/modulo/logic/parser/FindCommandParser.java
+++ src/main/java/modulo/logic/parser/FindCommandParser.java
@@ -17,19 +17,19 @@ public class FindCommandParser implements Parser<FindCommand> {
      * Parses the given {@code String} of arguments in the context of the FindCommand and returns a FindCommand object
      * for execution.
      *
+     * @param args Arguments passed in by the user.
+     * @return {@code FindCommand} to execute.
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindCommand parse(String args) throws ParseException {
         String trimmedArgs = args.toLowerCase().trim();
 
         if (trimmedArgs.isEmpty()) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
         String[] nameKeywords = {trimmedArgs};
 
         return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
     }
-
 }
