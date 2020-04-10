d3ea0c8372b4b68809faa9ddf23a6ce5ac397e73
Hanming Zhu <hanming@huawen.com>
2020-04-10 20:56:06 +0800

    docs: fix javadocs for Ics writer classes

--- src/main/java/modulo/logic/writer/IcsRRule.java
+++ src/main/java/modulo/logic/writer/IcsRRule.java
@@ -1,13 +1,20 @@
 package modulo.logic.writer;
 
 /**
- * Class that helps
+ * Class that helps to convert repeated events into .ics format.
  */
 public class IcsRRule {
     private String freq;
     private int count;
     private String byDay;
 
+    /**
+     * Constructs a helper class with the given parameters.
+     *
+     * @param freq  Frequency of repeat.
+     * @param count Times to repeat.
+     * @param byDay When to stop repeat.
+     */
     public IcsRRule(String freq, int count, String byDay) {
         this.freq = freq;
         this.count = count;
