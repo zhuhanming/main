daea35ae3f62b021d82904cff2bdb1d4b051a908
Hanming Zhu <hanming@huawen.com>
2020-04-10 20:57:19 +0800

    docs: fix javadocs for Model

--- src/main/java/modulo/model/Model.java
+++ src/main/java/modulo/model/Model.java
@@ -20,7 +20,7 @@ import modulo.model.module.ModuleCode;
  */
 public interface Model {
     /**
-     * {@code Predicate} that always evaluate to true
+     * {@code Predicates} for the two lists.
      */
     Predicate<Event> PREDICATE_SHOW_UPCOMING_EVENTS = event -> !event
             .getEventStart().toLocalDate().isBefore(LocalDate.now());
@@ -48,97 +48,116 @@ public interface Model {
     void setGuiSettings(GuiSettings guiSettings);
 
     /**
-     * Returns the user prefs' address book file path.
+     * Returns the user prefs' Modulo file path.
      */
-    Path getCalendarFilePath();
+    Path getModuloFilePath();
 
     /**
-     * Sets the user prefs' address book file path.
+     * Sets the user prefs' Modulo file path.
      */
-    void setCalendarFilePath(Path calendarFilePath);
+    void setModuloFilePath(Path moduloFilePath);
 
     /**
-     * Finds reference to existing calendar item
+     * Finds reference to existing event.
      */
     Event findEvent(Event event);
 
     /**
-     * Finds reference to existing calendar item
+     * Finds and returns all events that are similar to the event to find.
+     *
+     * @param toFind Event to find.
+     * @return List of all events.
+     */
+    List<Event> findAllEvents(Event toFind);
+
+    /**
+     * Finds reference to existing module.
      */
     Module findModule(Module module);
 
     /**
-     * Replaces address book data with the data in {@code addressBook}.
+     * Replaces Modulo data with the data in {@code modulo}.
      */
     void setModulo(ReadOnlyModulo modulo);
 
     /**
-     * Returns the AddressBook
+     * Returns Modulo.
      */
     ReadOnlyModulo getModulo();
 
     /**
-     * Returns true if a person with the same identity as {@code person} exists in the address book.
+     * Returns true if an event with the same identity as {@code event} exists in Modulo.
      */
     boolean hasEvent(Event event);
 
     /**
-     * Returns true if a person with the same identity as {@code person} exists in the address book.
+     * Returns true if a module with the given {@code moduleCode} and {@code academicYear} exists in Modulo.
      */
     boolean hasModule(ModuleCode moduleCode, AcademicYear academicYear);
 
     /**
-     * Deletes the given person. The person must exist in the address book.
+     * Deletes the given event. The event must exist in Modulo.
      */
     void deleteEvent(Event event);
 
     /**
-     * Deletes the given person. The person must exist in the address book.
+     * Deletes the given module. The module must exist in Modulo.
      */
     void deleteModule(Module module);
 
     /**
-     * Adds the given person. {@code person} must not already exist in the address book.
+     * Adds the given event. {@code event} must not already exist in Modulo.
      */
     void addEvent(Event event);
 
+    /**
+     * Adds a module to Modulo using the given {@code moduleCode} and {@code academicYear}.
+     *
+     * @param moduleCode   Module code of the module.
+     * @param academicYear Academic year of the module.
+     */
     void addModule(ModuleCode moduleCode, AcademicYear academicYear);
 
+    /**
+     * Gets an optional for the module with the given module code and academic year.
+     *
+     * @param moduleCode   Module code to look for.
+     * @param academicYear Academic year of the module.
+     * @return Optional of the required module.
+     */
     Optional<Module> getModule(ModuleCode moduleCode, AcademicYear academicYear);
 
     /**
-     * Replaces the given person {@code target} with {@code editedPerson}. {@code target} must exist in the address
-     * book. The person identity of {@code editedPerson} must not be the same as another existing person in the address
-     * book.
+     * Replaces the given event {@code target} with {@code editedEvent}. {@code target} must exist in Modulo. The event
+     * identity of {@code editedEvent} must not be the same as another existing event in Modulo.
      */
     void setEvent(Event target, Event editedEvent);
 
     /**
-     * Replaces the given person {@code target} with {@code editedPerson}. {@code target} must exist in the address
-     * book. The person identity of {@code editedPerson} must not be the same as another existing person in the address
-     * book.
+     * Replaces the given module {@code target} with {@code editedModule}. {@code target} must exist in Modulo. The
+     * module identity of {@code editedModule} must not be the same as another existing module in Modulo.
      */
     void setModule(Module target, Module editedModule);
 
     /**
-     * Returns an unmodifiable view of the filtered event list
+     * Returns an unmodifiable view of the filtered event list.
      */
     ObservableList<Event> getFilteredEventList();
 
     /**
-     * Returns an unmodifiable view of the filtered module list
+     * Returns an unmodifiable view of the filtered module list.
      */
     ObservableList<Module> getFilteredModuleList();
 
     /**
-     * Updates the filter of the filtered person list to filter by the given {@code predicate}.
+     * Updates the filter of the filtered event list to filter by the given {@code predicate}.
      *
      * @throws NullPointerException if {@code predicate} is null.
      */
     void updateFilteredEventList(Predicate<Event> predicate);
 
     /**
-     * Updates the filter of the filtered person list to filter by the given {@code predicate}.
+     * Updates the filter of the filtered module list to filter by the given {@code predicate}.
      *
      * @throws NullPointerException if {@code predicate} is null.
      */
@@ -151,11 +170,17 @@ public interface Model {
      */
     ObservableList<? extends Displayable> getFilteredFocusedList();
 
-
+    /**
+     * Gets the displayable type that is in focus.
+     *
+     * @return Displayable type that is in focus.
+     */
     Displayable getFocusedDisplayable();
 
     /**
-     * Set the list in focus.
+     * Sets the type of list to be in focus.
+     *
+     * @param displayableType Type of list to be shown.
      */
     void setFilteredFocusedList(DisplayableType displayableType);
 
@@ -171,20 +196,41 @@ public interface Model {
      */
     DisplayableType getCurrentDisplayableType();
 
-    public void setFilteredFocusedListShowAll(DisplayableType displayableType);
+    /**
+     * Sets the type of list to be in focus and show all items in that list.
+     *
+     * @param displayableType Type of list to be shown.
+     */
+    void setFilteredFocusedListShowAll(DisplayableType displayableType);
 
+    /**
+     * Checks if the focused displayable is the same as {@code displayable}.
+     *
+     * @param displayable Displayable to check.
+     * @return Boolean value denoting whether the focused displayable is the same as the one required.
+     */
     boolean isSameFocusedDisplayable(Displayable displayable);
 
+    /**
+     * Set a filter for the current displayable list.
+     *
+     * @param predicate Predicate to filter with.
+     */
     void updateFilteredDisplayableList(Predicate<Displayable> predicate);
 
+    /**
+     * Returns the filtered displayable list.
+     *
+     * @param predicate Predicate to filter with.
+     * @return List of the displayable with the filter applied.
+     */
     Object[] getFilteredDisplayableList(Predicate<Displayable> predicate);
 
+    /**
+     * Unsets the current focused displayable.
+     */
     void unsetFocusedDisplayable();
 
     //to be deleted after debugging
     String checkCurrentCalendar();
-
-    List<Event> findAllEvents(Event toFind);
-
-    Event getEvent(Event toFind);
 }
