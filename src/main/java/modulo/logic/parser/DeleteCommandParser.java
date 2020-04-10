0cbd115bec25a45ae43137ebc7d498c13ea0f290
Hanming Zhu <hanming@huawen.com>
2020-04-10 20:55:38 +0800

    docs: fix javadocs for DeleteCommandParser

--- src/main/java/modulo/logic/parser/DeleteCommandParser.java
+++ src/main/java/modulo/logic/parser/DeleteCommandParser.java
@@ -4,6 +4,7 @@ import static modulo.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static modulo.logic.parser.CliSyntax.PREFIX_DEADLINE;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.stream.Stream;
 
 import modulo.commons.core.index.Index;
@@ -37,7 +38,7 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
 
         if (arePrefixesPresent(argMultimap, PREFIX_DEADLINE)) {
             if (argMultimap.getValue(PREFIX_DEADLINE).get().equals("all")) {
-                return new DeleteCommand(new NameContainsKeywordsPredicate(Arrays.asList("")), true);
+                return new DeleteCommand(new NameContainsKeywordsPredicate(Collections.singletonList("")), true);
             }
             Index deadlineIndex = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_DEADLINE).get());
             return new DeleteCommand(deadlineIndex, true);
@@ -49,13 +50,21 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
             return new DeleteCommand(index, false);
         } catch (NumberFormatException e) {
             if (trimmedArgs.equals("all")) {
-                return new DeleteCommand(new NameContainsKeywordsPredicate(Arrays.asList("")), false);
+                return new DeleteCommand(new NameContainsKeywordsPredicate(Collections.singletonList("")), false);
             }
             String[] nameKeywords = {trimmedArgs};
             return new DeleteCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)), false);
         }
     }
 
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given {@code
+     * ArgumentMultimap}.
+     *
+     * @param argumentMultimap Multimap containing the arguments.
+     * @param prefixes         Prefixes to check.
+     * @return Boolean denoting whether the prefixes are present.
+     */
     private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
         return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
