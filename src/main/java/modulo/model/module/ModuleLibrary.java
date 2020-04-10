6a43faae916a973253c5f82d6974575a2259aac5
Hanming Zhu <hanming@huawen.com>
2020-04-10 20:57:39 +0800

    docs: fix javadocs for ModuleLibrary

--- src/main/java/modulo/model/module/ModuleLibrary.java
+++ src/main/java/modulo/model/module/ModuleLibrary.java
@@ -43,9 +43,7 @@ public class ModuleLibrary {
             Name name = new Name(moduleNeeded.get("title").getAsString());
             String description = moduleNeeded.get("description").getAsString();
             return new Module(moduleCode, name, academicYear, description);
-        } catch (NullPointerException e) {
-            throw new ModuleNotFoundException();
-        } catch (IOException e) {
+        } catch (NullPointerException | IOException e) {
             throw new ModuleNotFoundException();
         }
     }
@@ -75,6 +73,15 @@ public class ModuleLibrary {
         }
     }
 
+    /**
+     * Gets an add event command for execution, with the details loaded from the json files.
+     *
+     * @param module    Module which the event belongs to.
+     * @param eventType Event type to search for.
+     * @param eventSlot Slot of the event.
+     * @return {@code AddEventCommand} to execute.
+     * @throws EventNotFoundException Event cannot be found.
+     */
     public static AddEventCommand getAddEventCommandToExecute(Module module, EventType eventType,
                                                               String eventSlot) throws EventNotFoundException {
         try {
