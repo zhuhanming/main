6f1b34bf793fed456ddf4e925dced345d43b6315
Hanming Zhu <hanming@huawen.com>
2020-04-12 11:24:19 +0800

    test: add displayable tests

--- src/test/java/modulo/model/displayable/DisplayablePairTest.java
+++ src/test/java/modulo/model/displayable/DisplayablePairTest.java
@@ -1,4 +1,21 @@
 package modulo.model.displayable;
 
+import static modulo.testutil.module.TypicalModules.CS2103;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import modulo.model.module.Module;
+
+/**
+ * JUnit test class for {@code DisplayablePair}.
+ */
 public class DisplayablePairTest {
+
+    @Test
+    public void testConstructor_validInputs_successfulConstruction() {
+        DisplayablePair<Module, Integer> testDisplayablePair = new DisplayablePair<>(CS2103, 5);
+        assertEquals(testDisplayablePair.getFirst(), CS2103);
+        assertEquals(testDisplayablePair.getSecond(), 5);
+    }
 }
